80d9325ece49c1cea440f565844173eb8fe56aec
errcHuang <eric.huanghg@gmail.com>
2019-07-29 21:41:54 -0600

    app

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, 
                 external_stylesheets=external_stylesheets)
+server = app.server
 
 # indicator box  
 def indicator(text, value):
